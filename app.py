66555608c0773ef7ff0e5ed15f98b17956da2757
Marko <marko.cuk.888@gmail.com>
2022-06-02 10:29:17 +0200

    connect databse with app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import render_template
-
+from db_data import conn
 
 app = Flask(__name__)
 
 @app.route("/")
 def login():
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM admin;')
+    admin = cur.fetchall()
+    print(admin)
+    cur.close()
     return render_template('login.html')
 
 
